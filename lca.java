0d32dbeb594f965b46a4c4b7f8b08f1a80e202c5
varunwali <92247037+varunwali@users.noreply.github.com>
2023-06-05 22:37:55 +0530

    Create lca.java

---
+++ lca.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==0 || root==p || root==q ){
+            return root;
+        }
+        TreeNode* left = lowestCommonAncestor(root->left,p,q);
+        TreeNode* right = lowestCommonAncestor(root->right,p,q);
+        if(left==0){
+            return right;
+        }
+        else if(right==0){
+            return left;
+        }
+        else{
+            return root;
+        }
+    }
+};
